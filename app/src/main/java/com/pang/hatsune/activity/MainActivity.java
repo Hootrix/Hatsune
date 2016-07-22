cc825cb260e04e8391018c872f76195365797425
hootrix <hhtjim@yeah.net>
2016-07-22 18:00:28 +0800

    添加fragment   顶部标题

--- app/src/main/java/com/pang/hatsune/activity/MainActivity.java
+++ app/src/main/java/com/pang/hatsune/activity/MainActivity.java
@@ -1,9 +1,12 @@
 package com.pang.hatsune.activity;
 
-import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,19 +16,66 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.pang.hatsune.R;
+import com.pang.hatsune.fragment.Fragment1News;
+import com.pang.hatsune.fragment.Fragment2Channel;
+import com.pang.hatsune.fragment.Fragment3Echo;
+import com.pang.hatsune.fragment.Fragment4Celebrity;
 
-public class MainActivity extends Activity {//AppCompatActivity
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class MainActivity extends BaseActivity {//AppCompatActivity
+    NavigationView navigationView;//侧滑栏
+    DrawerLayout drawer;//整个视图布局  包含侧滑栏
+    RadioGroup radioGroupTop;//顶部导航条的radiogroup
+    RadioGroup radioGroupBottom;//底部的radiogroup
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+    ArrayList<Fragment> fragmentList;
+    Fragment1News fragment1News;
+    Fragment2Channel fragment2Channel;
+    Fragment3Echo fragment3Echo;
+    Fragment4Celebrity fragment4Celebrity;
+    TextView topTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-       // setSupportActionBar(toolbar);//设置toolbar
+//        setSupportActionBar(toolbar);//设置toolbar
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        radioGroupTop = (RadioGroup) this.findViewById(R.id.main_top_radiogroup);
+        radioGroupBottom = (RadioGroup) this.findViewById(R.id.main_bottom_radiogroup);
+        topTitle = (TextView) this.findViewById(R.id.main_top_title);
+        topTitle.setVisibility(View.GONE);//默认隐藏标题
+        fragmentManager = this.getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
 
+        RadioButton ra1  = (RadioButton) findViewById(R.id.main_top_radiogroup_r1);
+        RadioButton ra2  = (RadioButton) findViewById(R.id.main_top_radiogroup_r2);
+        Drawable drawable1 = getResources().getDrawable(R.drawable.main_top_title_radiogroup_selector1);
+        int wh = 50;
+        drawable1.setBounds(0, 0, wh, wh);//第一0是距左边距离，第二0是距上边距离，40分别是长宽
+        ra1.setCompoundDrawables(drawable1, null, null, null);//只放左边
 
+         drawable1 = getResources().getDrawable(R.drawable.main_top_title_radiogroup_selector2);
+        drawable1.setBounds(0, 0, wh, wh);//第一0是距左边距离，第二0是距上边距离，40分别是长宽
+        ra2.setCompoundDrawables(drawable1, null, null, null);//只放左边
+
+        setFragmentListData();
+        fragmentTransaction.add(R.id.main_fragment_viewgroup, fragment1News);
+        fragmentTransaction.add(R.id.main_fragment_viewgroup, fragment2Channel);
+        fragmentTransaction.add(R.id.main_fragment_viewgroup, fragment3Echo);
+        fragmentTransaction.add(R.id.main_fragment_viewgroup, fragment4Celebrity);
+        fragmentTransaction.commit(); //commit()方法提交对事务的操作
+        radioGroupBottom.check(R.id.main_bottom_radiogroup_r3);//默认点亮第三个radiobutton
         //监听浮动按钮
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -39,14 +89,39 @@ public class MainActivity extends Activity {//AppCompatActivity
         });
 
 
-        //绑定toolbar左边的菜单图标的动作
+////        绑定toolbar左边的菜单图标的动作
 //        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 //        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
 //            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 //        drawer.setDrawerListener(toggle);
+////        toggle.setDrawerIndicatorEnabled(false);
 //        toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        radioGroupBottom.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                Fragment currentFragment = null;
+                switch (checkedId) {
+                    case R.id.main_bottom_radiogroup_r1:
+                        currentFragment = fragment1News;
+                        break;
+                    case R.id.main_bottom_radiogroup_r2:
+                        currentFragment = fragment2Channel;
+                        break;
+                    case R.id.main_bottom_radiogroup_r3:
+                        currentFragment = fragment3Echo;
+                        break;
+                    case R.id.main_bottom_radiogroup_r4:
+                        currentFragment = fragment4Celebrity;
+                        break;
+                    default:
+                        return;
+                }
+                displayFragment(currentFragment);
+            }
+        });
+
 
         /**
          * 监听侧滑栏的item 点击
@@ -71,25 +146,13 @@ public class MainActivity extends Activity {//AppCompatActivity
 
                 }
 
-                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                //  DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
                 drawer.closeDrawer(GravityCompat.START);
                 return true;
             }
         });
     }
-//
-//    /**
-//     *  监听后退键
-//     */
-//    @Override
-//    public void onBackPressed() {
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        if (drawer.isDrawerOpen(GravityCompat.START)) {
-//            drawer.closeDrawer(GravityCompat.START);
-//        } else {
-//            super.onBackPressed();
-//        }
-//    }
+
 
 //
 //    @Override
@@ -115,4 +178,66 @@ public class MainActivity extends Activity {//AppCompatActivity
 //    }
 
 
+    /**
+     * 监听头像的点击 然后打开或关闭侧滑栏
+     *
+     * @param v
+     */
+    public void toggle_left_view(View v) {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {//如果侧滑栏已打开
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            drawer.openDrawer(GravityCompat.START);
+        }
+    }
+
+
+    /**
+     * 把fragment装进容器
+     */
+    public void setFragmentListData() {
+        fragmentList = new ArrayList<Fragment>();
+        fragment1News = new Fragment1News();
+        fragment2Channel = new Fragment2Channel();
+        fragment3Echo = new Fragment3Echo();
+        fragment4Celebrity = new Fragment4Celebrity();
+        fragmentList.add(fragment1News);
+        fragmentList.add(fragment2Channel);
+        fragmentList.add(fragment3Echo);
+        fragmentList.add(fragment4Celebrity);
+    }
+
+    /**
+     * 显示 隐藏fragment
+     *
+     * @param fragment
+     */
+    public void displayFragment(Fragment fragment) {//传入需要显示的Fragment
+        showHideTopBar(fragment);
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.show(fragment);
+        Iterator<Fragment> it = fragmentList.iterator();
+        while (it.hasNext()) {
+            Fragment f = it.next();
+            if (f != fragment) {
+                fragmentTransaction.hide(f);
+            }
+        }
+        fragmentTransaction.commit();
+    }
+
+
+    /**
+     * 显示隐藏顶部导航标题
+     */
+    public void showHideTopBar(Fragment fragment) {
+        if (fragment instanceof Fragment3Echo) {
+            radioGroupTop.setVisibility(View.VISIBLE);
+            topTitle.setVisibility(View.GONE);
+        } else {
+            radioGroupTop.setVisibility(View.GONE);
+            topTitle.setVisibility(View.VISIBLE);
+        }
+    }
 }
