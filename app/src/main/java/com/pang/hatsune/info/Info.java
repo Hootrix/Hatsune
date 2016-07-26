3beec12c8ee58c6bff00368dab3c5b57f036e6d4
hootrix <hhtjim@yeah.net>
2016-07-26 09:50:12 +0800

    commit test

--- app/src/main/java/com/pang/hatsune/info/Info.java
+++ app/src/main/java/com/pang/hatsune/info/Info.java
@@ -3,7 +3,7 @@ package com.pang.hatsune.info;
 import java.io.Serializable;
 
 /**
- * Created by Administrator on 2016/7/25.
+ * Created by Administrator on 2016/7/25...
  */
  interface Info extends Serializable{
 
